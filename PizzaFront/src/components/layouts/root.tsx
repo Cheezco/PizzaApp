71b4338623f959073f4a48a754e4f463071abdaa
Cheezco <60855273+Cheezco@users.noreply.github.com>
2023-10-22 19:30:44 +0300

    Update root layout

--- PizzaFront/src/components/layouts/root.tsx
+++ PizzaFront/src/components/layouts/root.tsx
@@ -1,9 +1,34 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function Root() {
   return (
-    <div className="d-flex justify-content-center w-100 h-100 align-items-center">
-      <Outlet />
+    <div className="h-100 w-100 d-flex flex-column">
+      <Navbar className="position-sticky top-0">
+        <Container fluid>
+          <LinkContainer to="/">
+            <Navbar.Brand>PizzaMe</Navbar.Brand>
+          </LinkContainer>
+          <Nav>
+            <LinkContainer to="/pizza-creator">
+              <Nav.Link>Create your pizza</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="orders">
+              <Nav.Link>Orders</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="register">
+              <Nav.Link>Register</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="d-flex w-100 h-100">
+        <Outlet />
+      </div>
     </div>
   );
 }
